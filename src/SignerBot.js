f6ba3d6cea598c618a12b147ce57f98c5bc3d30a
rotciv <victorfage@gmail.com>
2020-12-16 23:39:10 -0300

    log orders live

--- src/SignerBot.js
+++ src/SignerBot.js
@@ -8,6 +8,7 @@ module.exports = class SignerBot {
   async process () {
     for (;;) {
       const keys = await process.redis.getKeysAsync('order:*');
+      console.log('Orders live:', keys.length);
 
       for (let i = 0; i < keys.length; i++) {
         const order = JSON.parse(await process.redis.getValue(keys[i]));
